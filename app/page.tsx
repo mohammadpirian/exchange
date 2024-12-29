53f596374174dbf004481289d85f67ac114cac43
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-29 11:12:14 +0330

    add Image [Home]

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import SignupForm from "@/modules/signup/formSignUp";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen gap-16">
+      <main className="w-full h-full grid grid-cols-2 justify-center items-center gap-20">
+        <div className="w-full flex justify-end">
+          <Image src="loginBanner.svg" alt="Logo" width={428} height={428} />
+        </div>
         <SignupForm />
       </main>
     </div>
